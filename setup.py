66f47939b58ae13df5038ff4dbdee23fc97b3fb3
SunitaRaj101 <sunitafou9@gmail.com>
2023-11-05 15:04:12 +0530

    First setup commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages , setup
+
+
+
+
+
+
+
+
+setup(
+    name="mlproject",
+    version='0.0.1',
+    author='Sunita Singh',
+    author_email='sunitafou9@gmail.com',
+    packages=find_packages(),
+    install_requires=['numpy','pandas','flask']
+)   
